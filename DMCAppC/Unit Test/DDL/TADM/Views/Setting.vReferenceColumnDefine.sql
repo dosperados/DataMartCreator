74ae3a608b01e62fe5862d55c1a41eb1b91c2bff
Vitali <41899258+dosperados@users.noreply.github.com>
2026-03-02 17:07:35 +0100

    Add files via upload

---
+++ DMCAppC/Unit Test/DDL/TADM/Views/Setting.vReferenceColumnDefine.sql
@@ -0,0 +1,47 @@
+﻿
+
+CREATE VIEW [Setting].[vReferenceColumnDefine]
+	AS 
+	SELECT RefColDef.[ReferenceColumnDefineId]
+          ,RefColDef.[VersionId]
+          ,RefColDef.[SourceReferenceTableDefineId]
+          ,RefColDef.[SourceTableName]
+          ,SourRefTabDef.[SourceMainTableName]
+          ,SourRefTabDef.[SourceMainSchemaName]
+          ,SourRefTabDef.[SourceMainDBName]
+          ,SourRefTabDef.[RefTableAliasName]
+          ,SourRefTabDef.[RefLevelNo]
+          ,SourRefTabDef.[RefTableAliasName]+SourRefTabDef.[RefLevelNo] AS [RefTableAlias]
+          --,SourRefTabDef.[CustomeTableSelect]
+          ,SourRefTabDef.[JoinType]
+          ,SourRefTabDef.[RefType]
+          ,SourRefTabDef.[DestMainTableName]
+          ,RefColDef.[SourceColumnId]
+          ,SourCol.[ColumnName] as [SourceColumnName]
+          ,SourCol.[FieldType] as [SourceFieldType]
+          ,SourCol.[IsOptionField] as [SourceIsOptionField]
+          ,RefColDef.[SourceColumnFunction]
+
+          ,RefColDef.[RefReferenceTableDefineId]
+          ,RefColDef.[RefTableName]
+          ,LookupRefTabDef.[SourceMainTableName] as [RefSourceMainTableName]
+          ,RefColDef.[RefColumnId]
+          ,LookupCol.[ColumnName] as [RefColumnName]
+          ,RefColDef.[RefColumnName] as [ReferenceColumnDefine.RefColumnName]
+          ,LookupRefTabDef.[RefTableAliasName]+LookupRefTabDef.[RefLevelNo] AS [RefReferenceTableAlias]
+          ,RefColDef.[RefColumnFunction]
+          ,RefColDef.[KeySortNo]
+          ,RefColDef.[Description]
+		  ,RefColDef.[Deleted]
+			 
+	FROM [Setting].[ReferenceColumnDefine] as RefColDef
+		INNER JOIN [Setting].[vReferenceTableDefine] as SourRefTabDef
+			ON RefColDef.[SourceReferenceTableDefineId] = SourRefTabDef.[ReferenceTableDefineId]
+        INNER JOIN [Setting].[vColumn] as SourCol
+            ON RefColDef.[SourceColumnId] = SourCol.[ColumnId]
+
+		INNER JOIN [Setting].[vReferenceTableDefine] as LookupRefTabDef
+			ON RefColDef.[RefReferenceTableDefineId] = LookupRefTabDef.[ReferenceTableDefineId]
+        INNER JOIN [Setting].[vColumn] as LookupCol
+            ON RefColDef.[RefColumnId] = LookupCol.[ColumnId]
+	WHERE RefColDef.[Deleted] = 0
---
+++ DMCAppC/Unit Test/DDL/TADM/Views/Setting.vTableDefine.sql
@@ -0,0 +1,28 @@
+﻿CREATE VIEW [Setting].[vTableDefine]
+	AS 
+	SELECT   TD.[TableDefineId]
+			,TD.[VersionId]
+			,V.[Description] 
+			,TD.[SourceMainDBId]
+			,SDB.[DBName] as [SourceDBName]
+			,TD.[SourceMainTableId]
+			,ST.[SchemaName] as [SourceSchemaName]
+			,ST.[TableName] as [SourceTableName] 
+			,TD.[DestMainDBId]
+			,DDB.[DBName] as [DestDBName]
+			,TD.[DestMainTableId]
+			,DT.[SchemaName] as [DestSchemaName]
+			,DT.[TableName] as [DestTableName]
+	
+	FROM [Setting].[TableDefine] as TD
+		INNER JOIN [Setting].[Version] as V
+			ON TD.[VersionId] = V.[VersionId]
+		INNER JOIN [Setting].[DB] as SDB
+			ON TD.[SourceMainDBId] = SDB.[DBId]
+		INNER JOIN [Setting].[Table] as ST
+			ON TD.[SourceMainTableId] = st.[TableId]
+		INNER JOIN [Setting].[DB] as DDB
+			ON TD.[DestMainDBId] = DDB.[DBId]
+		INNER JOIN [Setting].[Table] as DT
+			ON TD.[DestMainTableId] = DT.[TableId]
+
