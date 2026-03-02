bfcaa9def0a3edfbd6efd5148959c7ffa60a9ddb
Vitali <41899258+dosperados@users.noreply.github.com>
2026-03-02 17:01:49 +0100

    Delete DMCAppC/Unit Test directory

--- DMCAppC/Unit Test/DDL/TADM/Tables/Setting/Setting.DefaultColumnBlackList.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [Setting].[DefaultColumnBlackList]
-(
-	[DefaultColumnBlackListId] INT NOT NULL PRIMARY KEY IDENTITY(200,1),
-	[ColumnName] NVARCHAR(128) NULL,
-	[ModifiedAt]  DATETIME        CONSTRAINT [df_Setting_DefaultColumnBlackList_ModifiedAt] DEFAULT (getdate()) NOT NULL,
-    [ModifiedBy]  [sysname]       CONSTRAINT [df_Setting_DefaultColumnBlackList_ModifiedBy] DEFAULT ('SYSTEM') NOT NULL,
-    [Deleted]     TINYINT         CONSTRAINT [df_Setting_DefaultColumnBlackList_Deleted] DEFAULT ((0)) NOT NULL,
-	CONSTRAINT [UQ_Setting_DefaultColumnBlackList_ColumnName] UNIQUE ([ColumnName])
-)
